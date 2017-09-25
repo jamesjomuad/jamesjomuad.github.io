aee709219a46b2c2a69db3f73377df1b355d7262
James <st.james.jomuad@gmail.com>
2017-09-25 09:10:55 +0800

    Awards

---
+++ assets/demo/ng-app-challenge/versions/7-16-2015/js/helpers.js
@@ -0,0 +1,51 @@
+//
+//	Helpers
+//
+Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+}
+Storage.prototype.getObject = function(key) {
+    var value = this.getItem(key);
+    return value && JSON.parse(value);
+}
+function reset(){
+	localStorage.clear();
+	return "Storage Clear";
+}
+
+
+//
+//	JQuery
+//
+$(document).ready(function(){
+	
+	$(document).on('click', '[role=tab]', function (e) {
+		e.preventDefault();
+		window.location.hash = $(this).attr('href');
+		$('.grid').isotope('layout');
+	});
+
+	$('.grid').isotope({
+		itemSelector: '.box',
+		layoutMode: 'fitRows'
+	}); 
+
+	$(".datepicker").datepicker('update', new Date());
+	
+	$(".month-year").datepicker( {
+		format: "mm-yyyy",
+		startView: "months", 
+		minViewMode: "months"
+	}).datepicker('update', new Date());
+
+});
+
+$(window).load(function(){
+	if(window.location.hash)
+		$('a[href="'+window.location.hash+'"]').tab('show');
+	else
+		$('a[href=#home]').tab('show');
+
+	// $('.grid').isotope('layout');
+});
+
---
+++ assets/demo/ng-app-challenge/versions/7-16-2015/readme.txt
@@ -0,0 +1,12 @@
+Welcome!
+
+Browser Budget Manager will simply manages your budget.
+Browser Budget Manager help you to track your expenses, incomes, savings and always stay with-in budget.
+It helps you visualize and give you idea about your budget.
+
+URL: http://app.jamesjomuad.com/
+Copyright 2015 by James Jomuad
+
+
+Resources:
+http://bootsnipp.com/snippets/featured/timeline-dotted
