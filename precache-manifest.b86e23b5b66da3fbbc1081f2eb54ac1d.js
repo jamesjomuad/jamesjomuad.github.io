90b8d7468b5256298ccf9f5a55927b49a8f0334b
james jomuad <st.james.jomuad@gmail.com>
2020-02-10 16:54:20 +0800

    New React Portfolio

---
+++ precache-manifest.b86e23b5b66da3fbbc1081f2eb54ac1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f53817a22ddd218ad10272e7a2844bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ccda507203442eb7c015",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7f2edafc934b0eba4327",
+    "url": "/static/css/main.5d6ab2e3.chunk.css"
+  },
+  {
+    "revision": "ccda507203442eb7c015",
+    "url": "/static/js/2.e4faa183.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.e4faa183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2edafc934b0eba4327",
+    "url": "/static/js/main.084c3fc0.chunk.js"
+  },
+  {
+    "revision": "77d451e7038c8864e2b2",
+    "url": "/static/js/runtime-main.6e48ae5f.js"
+  },
+  {
+    "revision": "9abc7d4f0a67ac6157cab0b0fd9480c1",
+    "url": "/static/media/profile.9abc7d4f.jpg"
+  }
+]);
